e2423818437eff38f08758075035242980a918da
wangguoxin <657826620@qq.com>
2017-08-25 08:48:46 +0800

    对于解决redeployed时候mysql驱动被强制注销的问题，所做的尝试。

---
+++ out/artifacts/Lincang_war_exploded/js/app.js
@@ -0,0 +1,192 @@
+$(document).ready(function () {
+    //themes, change CSS with JS
+    //default theme(CSS) is cerulean, change it if needed
+    var defaultTheme = 'cerulean';
+    var currentTheme = $.cookie('currentTheme') == null ? defaultTheme : $.cookie('currentTheme');
+    var msie = navigator.userAgent.match(/msie/i);
+    $.browser = {};
+    $.browser.msie = {};
+    switchTheme(currentTheme);
+
+    $('.navbar-toggle').click(function (e) {
+        e.preventDefault();
+        $('.nav-sm').html($('.navbar-collapse').html());
+        $('.sidebar-nav').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+    var $sidebarNav = $('.sidebar-nav');
+
+    // Hide responsive navbar on clicking outside
+    $(document).mouseup(function (e) {
+        if (!$sidebarNav.is(e.target) // if the target of the click isn't the container...
+            && $sidebarNav.has(e.target).length === 0
+            && !$('.navbar-toggle').is(e.target)
+            && $('.navbar-toggle').has(e.target).length === 0
+            && $sidebarNav.hasClass('active')
+            )// ... nor a descendant of the container
+        {
+            e.stopPropagation();
+            $('.navbar-toggle').click();
+        }
+    });
+
+
+    $('#themes a').click(function (e) {
+        e.preventDefault();
+        currentTheme = $(this).attr('data-value');
+        $.cookie('currentTheme', currentTheme, {expires: 365});
+        switchTheme(currentTheme);
+    });
+
+
+    function switchTheme(themeName) {
+        if (themeName == 'classic') {
+            $('#bs-css').attr('href', 'bower_components/bootstrap/dist/css/bootstrap.min.css');
+        } else {
+            $('#bs-css').attr('href', 'css/bootstrap-' + themeName + '.min.css');
+        }
+
+        $('#themes i').removeClass('glyphicon glyphicon-ok whitespace').addClass('whitespace');
+        $('#themes a[data-value=' + themeName + ']').find('i').removeClass('whitespace').addClass('glyphicon glyphicon-ok');
+    }
+
+    //disbaling some functions for Internet Explorer
+    if (msie) {
+        $('#is-ajax').prop('checked', false);
+        $('#for-is-ajax').hide();
+        $('#toggle-fullscreen').hide();
+        $('.login-box').find('.input-large').removeClass('span10');
+
+    }
+
+    //highlight current / active link
+    $('ul.main-menu li a').each(function () {
+        if ($($(this))[0].href == String(window.location))
+            $(this).parent().addClass('active');
+    });
+
+    //establish history variables
+    var
+        History = window.History, // Note: We are using a capital H instead of a lower h
+        State = History.getState(),
+        $log = $('#log');
+    //bind to State Change
+    History.Adapter.bind(window, 'statechange', function () { // Note: We are using statechange instead of popstate
+        var State = History.getState(); // Note: We are using History.getState() instead of event.state
+        $.ajax({
+            url: State.url,
+            success: function (msg) {
+                $('#content').html($(msg).find('#content').html());
+                $('#loading').remove();
+                $('#content').fadeIn();
+                var newTitle = $(msg).filter('title').text();
+                $('title').text(newTitle);
+                docReady();
+            }
+        });
+    });
+
+    $('.accordion > a').click(function (e) {
+        e.preventDefault();
+        var $ul = $(this).siblings('ul');
+        var $li = $(this).parent();
+        if ($ul.is(':visible')) $li.removeClass('active');
+        else                    $li.addClass('active');
+        $ul.slideToggle();
+    });
+
+    $('.accordion li.active:first').parents('ul').slideDown();
+
+    //other things to do on document ready, separated for ajax calls
+    docReady();
+
+
+
+});
+
+
+function docReady() {
+    //prevent # links from moving to top
+    $('a[href="#"][data-top!=true]').click(function (e) {
+        e.preventDefault();
+    });
+
+    //notifications
+    $('.noty').click(function (e) {
+        e.preventDefault();
+        var options = $.parseJSON($(this).attr('data-noty-options'));
+        noty(options);
+    });
+
+    //chosen - improves select
+    $('[data-rel="chosen"],[rel="chosen"]').chosen();
+
+    //tabs
+    $('#myTab a:first').tab('show');
+    $('#myTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    //tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //popover
+    $('[data-toggle="popover"]').popover();
+
+    //iOS / iPhone style toggle switch
+    $('.iphone-toggle').iphoneStyle();
+
+    //datatable
+    var originalFormTable;
+    originalFormTable = $('#originalForm').DataTable({
+        ajax: {
+            url: "/oform"
+        },
+        "order": [[2, 'asc']],
+        "serverSide": true,
+        "columns": [
+            {"data": "OID"},
+            {"data": "TITLE"},
+            {"data": "CREATED_AT"},
+            {"data": null}
+        ],
+
+        "language": {
+            "lengthMenu": "每页_MENU_ 条记录",
+            "zeroRecords": "没有找到记录",
+            "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+            "infoEmpty": "无记录",
+            "search": "搜索：",
+            "infoFiltered": "(从 _MAX_ 条记录过滤)",
+            "paginate": {
+                "previous": "上一页",
+                "next": "下一页"
+            }
+        }
+
+    });
+
+    //添加序号
+    //不管是排序，还是分页，还是搜索最后都会重画，这里监听draw事件即可
+    originalFormTable.on('draw.dt',function() {
+        originalFormTable.column(0, {
+            search: 'applied',
+            order: 'applied'
+        }).nodes().each(function(cell, i) {
+            //i 从0开始，所以这里先加1
+            i = i+1;
+            //服务器模式下获取分页信息
+            var page = table.page.info();
+            //当前第几页，从0开始
+            var pageno = page.page;
+            //每页数据
+            var length = page.length;
+            //行号等于 页数*每页数据长度+行号
+            var columnIndex = (i+pageno*length);
+            cell.innerHTML = columnIndex;
+        });
+    }).draw();
+
+}
