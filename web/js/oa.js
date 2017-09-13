2a2b42626768c739ed983b5cb2efe558cb131cbb
WJYman <1615911255@qq.com>
2017-09-13 14:52:39 +0800

    查看删除前台代码的编写

--- web/js/oa.js
+++ web/js/oa.js
@@ -25,23 +25,24 @@ function del_file(number) {
 }
 
 
+    function wipeData() {
+        $("#input1").val(" ");
+        $("#input2").val(" ");
+        $("#input3").val(" ");
+        $("#input4").val(" ");
+        $("#input5").val(" ");
+        $("#input6").val(" ");
+        $("#input8").val(" ");
+        $("#input9").val(" ");
+        $("#input10").val(" ");
+        $(".add_file").remove();
+    }
+
+
+
     //表单放弃
     $(".btn-danger").click(function () {
-
-         $("#input1").val(" ");
-         $("#input2").val(" ");
-         $("#input3").val(" ");
-         $("#input4").val(" ");
-         $("#input5").val(" ");
-         $("#input6").val(" ");
-         $("#input7").val(" ");
-         $("#input8").val(" ");
-         $("#input9").val(" ");
-         $("#input10").val(" ");
-
-         $(".add_file").remove();
-
-
+            wipeData()
     });
 
     // 表单保存
@@ -57,6 +58,10 @@ function del_file(number) {
         var content=  $("#input10").val();
         var attachment= $("#filesUpload > span");
         var arrAttachment=[];
+
+        var id=$("#oId").text();
+        var created_at=$("#created_at").text();
+
         for(var i=0;i<attachment.length;i++){
             arrAttachment.push(attachment.eq(i).text());
         }
@@ -67,18 +72,12 @@ function del_file(number) {
             "print":print,
             "revision":revision,
             "copy":copy,
-            /*"arrAttachment":arrAttachment,*/
             "keyword":keyword,
             "title":title,
-            "content":content
+            "content":content,
+            "id":id,
+            "created_at":created_at,
         };
-
-
-
-
-
-
-
 
         $.ajax({
             url: '/saveFormData.do',
@@ -88,11 +87,17 @@ function del_file(number) {
             async: false,
             contentType: "application/x-www-form-urlencoded; charset=utf-8",
             success: function (data) {
-                //location.reload();
-                alert("ok");
-                $(".btn-danger").click();
+
+                console.log(data);
+
+                alert("保存成功");
+                wipeData();
+
+
+
             }
         });
+
        /* $("#fileForm").submit();*/
        $("#fileForm").ajaxSubmit({
            url: '/file/multipleUpload.do',
@@ -101,17 +106,14 @@ function del_file(number) {
            dataType:'json',
            success: function (data)
            {
-
            },
            error: function (data)
            {
-
            }
        })
     });
 
     // 表单提交
-
     $(".btn-primary").click(function () {
 
         var dept=  $("#input1").val();
@@ -130,8 +132,6 @@ function del_file(number) {
         }
 
         var Id=$("#formId").val();
-
-        console.log($("#formId").val());
 
 
         var datas= {
@@ -167,9 +167,10 @@ function del_file(number) {
                     console.log(dept);
                     //location.reload();
                     alert("ok");
-                    $(".btn-danger").click();
+                    wipeData();
                 }
             });
+
             $("#fileForm").submit();
 
 
@@ -191,11 +192,6 @@ function detail(that) {
     var time = $(that).parents("tr").children("td:nth-child(3)").text();
 
 
-    $('#form_stuff').modal('show');
-
-
-
-
     $.ajax({
         url: '/queryStuffById.do',
         type: 'post',
@@ -204,19 +200,42 @@ function detail(that) {
         async: false,
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
         success: function (data) {
-            //location.reload();
-            console.log(data)
-
+            console.log(data);
+            $("#input1").val(data.dept);
+            $("#input2").val(data.author);
+            $("#input3").val(data.reviewer);
+            $("#input4").val(data.print);
+            $("#input5").val(data.revision);
+            $("#input6").val(data.copy);
+            $("#input8").val(data.keyword);
+            $("#input9").val(data.title);
+            $("#input10").val(data.content);
+            $("#oId").text(data.id);
+            $("#created_at").text(data.created_at);
+            $('#form_stuff').modal('show');
         }
-
-
     })
 
 
+}
 
 
 
-
+function deleteOrder(that) {
+    var oid = $(that).parents("tr").children("td:nth-child(1)").text();
+    $.ajax({
+        url: '/deleteStuffById.do',
+        type: 'post',
+        data: "id="+oid,
+        dataType: 'json',
+        async: false,
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        success: function (data) {
+            console.log(data);
+            $(that).parents("tr").remove();
+              alert("删除成功")
+        }
+    })
 }
 
 
