97198d659007c060b2c8a2bae6d1c9e833e0ca29
wangguoxin <657826620@qq.com>
2017-10-17 15:33:16 +0800

    查看和编辑的后端代码的编写

--- src/main/java/com/bhidi/lincang/controller/CapitalFlowController.java
+++ src/main/java/com/bhidi/lincang/controller/CapitalFlowController.java
@@ -1,5 +1,6 @@
 package com.bhidi.lincang.controller;
 
+import com.bhidi.lincang.bean.CapitalFlow;
 import com.bhidi.lincang.service.CapitalFlowServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,7 +18,6 @@ public class CapitalFlowController {
     @Autowired
     CapitalFlowServiceImp capitalFlowServiceImp;
 
-
     @ResponseBody
     @RequestMapping(value="/submitDataOfCapital",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public String submitDataOfCapital(String report_person,String report_quarter,String report_text) {
@@ -31,6 +31,46 @@ public class CapitalFlowController {
         map.put("report_text",report_person == null?"":report_text);
         map.put("status","已经提交！");
         int result = capitalFlowServiceImp.submitData(map);
+        return result+"";
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/getDataById",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public String getDataById(String id) {
+        CapitalFlow result = capitalFlowServiceImp.getDataById(id);
+        return result+"";
+    }
+
+    /**
+     * 点击了编辑之后的提交按钮
+     * @param id
+     * @param report_person
+     * @param report_quarter
+     * @param report_text
+     * @param money_source
+     * @param arrival_time
+     * @param amount
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value="/setDataById",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public String setDataById(String id,String create_time,String report_person,String report_quarter,String report_text,
+                              String money_source,String arrival_time,String amount) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        String finance_time = format.format(now);
+        Map<String,String> map = new HashMap();
+        map.put("create_time",create_time);
+        map.put("report_person",report_person == null?"":report_person);
+        map.put("report_quarter",report_quarter == null?"":report_quarter);
+        map.put("report_text",report_person == null?"":report_text);
+        map.put("money_source",money_source);
+        map.put("arrival_time",arrival_time);
+        map.put("amount",amount);
+        map.put("finance_time",finance_time);
+        map.put("status","市局规划科处理中");
+
+        int result = capitalFlowServiceImp.setDataById(map);
         return result+"";
     }
 }
--- src/main/java/com/bhidi/lincang/dao/CapitalFlowMapper.java
+++ src/main/java/com/bhidi/lincang/dao/CapitalFlowMapper.java
@@ -1,5 +1,6 @@
 package com.bhidi.lincang.dao;
 
+import com.bhidi.lincang.bean.CapitalFlow;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -7,4 +8,8 @@ import java.util.Map;
 @Repository
 public interface CapitalFlowMapper {
     int submitData(Map map);
+
+    CapitalFlow queryDataById(String id);
+
+    int updateDataById(Map<String, String> map);
 }
