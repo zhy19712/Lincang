81608c5fa0a9d7e5a740c0cd2ebadeaeec772614
wangguoxin <657826620@qq.com>
2017-10-21 12:26:12 +0800

    commit

--- src/main/java/com/bhidi/lincang/bean/User.java
+++ src/main/java/com/bhidi/lincang/bean/User.java
@@ -1,9 +1,12 @@
 package com.bhidi.lincang.bean;
 
+import java.util.List;
+
 /**
  * Created by admin on 2017/8/28.
  */
 public class User {
+    private int id;
     private String uid;
     private String username;
     private String password;
@@ -13,11 +16,17 @@ public class User {
     private String phone2;
     private int level;
     private String created_at;
+    //下边是和数据库不一样的属性
+    //角色名字
+    private List<String> roleList;
+    //权限名字
+    private List<String> permissionList;
 
     public User() {
     }
 
-    public User(String uid, String username, String password, String name, String dept, String phone1, String phone2, int level, String created_at) {
+    public User(int id, String uid, String username, String password, String name, String dept, String phone1, String phone2, int level, String created_at, List<String> roleList, List<String> permissionList) {
+        this.id = id;
         this.uid = uid;
         this.username = username;
         this.password = password;
@@ -27,6 +36,24 @@ public class User {
         this.phone2 = phone2;
         this.level = level;
         this.created_at = created_at;
+        this.roleList = roleList;
+        this.permissionList = permissionList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 
     public String getUsername() {
@@ -43,14 +70,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
     }
 
     public String getName() {
@@ -99,5 +118,21 @@ public class User {
 
     public void setCreated_at(String created_at) {
         this.created_at = created_at;
+    }
+
+    public List<String> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<String> roleList) {
+        this.roleList = roleList;
+    }
+
+    public List<String> getPermissionList() {
+        return permissionList;
+    }
+
+    public void setPermissionList(List<String> permissionList) {
+        this.permissionList = permissionList;
     }
 }
