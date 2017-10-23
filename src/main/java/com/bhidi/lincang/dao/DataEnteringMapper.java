26aa858a3645ac39166a9f3e398abaa06399dcaa
wangguoxin <657826620@qq.com>
2017-10-23 18:10:17 +0800

    后端

--- src/main/java/com/bhidi/lincang/dao/DataEnteringMapper.java
+++ src/main/java/com/bhidi/lincang/dao/DataEnteringMapper.java
@@ -2,6 +2,9 @@ package com.bhidi.lincang.dao;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DataEnteringMapper {
+    int insertPeople(List peopleList);
 }
