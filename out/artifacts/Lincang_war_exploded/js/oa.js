88cc4dd5de5597d6e1afbc87481fcaff6115c9c9
zhangchuan <993090861@qq.com>
2017-10-31 17:19:06 +0800

    发文

--- out/artifacts/Lincang_war_exploded/js/oa.js
+++ out/artifacts/Lincang_war_exploded/js/oa.js
@@ -1,7 +1,15 @@
-//待办表单的datatable
+
+
+//打印
+// $("#btn-print").click(function () {
+//     jQuery('#fileForm').print();
+// })
+
+// 全部列表datatables
+
 // var newForm_stuff = $('#NewTable_Stuff').DataTable({
 //     ajax: {
-//         url: "/nform_stuff.do",
+//         url: "/nform_stuff.do"
 //     },
 //     "order": [[2, 'asc']],
 //     "serverSide": true,
@@ -17,8 +25,7 @@
 //             "orderable": false,
 //             "targets": [3],
 //             "render" :  function(data,type,row) {
-//                 var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='detail(this)' value='查看'/>"
-//                 html += "<input type='button' class='btn btn-danger btn-xs' style='margin-left: 5px;' onclick='deleteOrder(this)' value='删除'/>"
+//                 var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='detail(this)' value='查看'/>";
 //                 return html;
 //             }
 //         },
@@ -42,8 +49,53 @@
 //     }
 // });
 
-//再办表单的datatable
-// var submittedForm_stuff = $('#SubmittedTable_Stuff').DataTable({
+
+//待办列表的datatables
+// var newForm_office = $('#NewTable_Office').DataTable({
+//     ajax: {
+//         url: "/nform_office.do",
+//         async:false
+//     },
+//     "order": [[1, 'asc']],
+//     "serverSide": true,
+//     "columns": [
+//         {"data": "OID"},
+//         {"data": "CREATED_AT"},
+//         {"data": "DEPT"},
+//         {"data": "AUTHOR"},
+//         {"data": "TITLE"},
+//         {"data": null}
+//     ],
+//     "columnDefs": [
+//         {
+//             "searchable": false,
+//             "orderable": false,
+//             "targets": [5],
+//             "render" :  function(data,type,row) {
+//                 var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' value='查看'/>";
+//                 html += "<input type='button' class='btn btn-warning btn-xs' style='margin-left: 5px;'  value='编辑'/>" ;
+//                 return html;
+//             }
+//         }
+//     ],
+//     "language": {
+//         "lengthMenu": "每页_MENU_ 条记录",
+//         "zeroRecords": "没有找到记录",
+//         "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+//         "infoEmpty": "无记录",
+//         "search": "搜索：",
+//         "infoFiltered": "(从 _MAX_ 条记录过滤)",
+//         "paginate": {
+//             "previous": "上一页",
+//             "next": "下一页"
+//         }
+//     }
+// });
+
+
+// 已办表单的datatables
+
+// var SubmittedTable_Office = $('#SubmittedTable_Office').DataTable({
 //     ajax: {
 //         url: "/sform_stuff.do"
 //     },
@@ -61,8 +113,8 @@
 //             "orderable": false,
 //             "targets": [3],
 //             "render" :  function(data,type,row) {
-//                 var html = "<input type='button' class='btn btn-info btn-xs' style='margin-left: 5px;' onclick='flow(this)' value='流程'/>" +
-//                     "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' disabled='disabled' onclick='' value='确认'/>";
+//                 var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='detail(this)' value='编辑'/>"
+//                 html += "<input type='button' class='btn btn-warning btn-xs' style='margin-left: 5px;' onclick='flow(this)' value='流程'/>" ;
 //                 return html;
 //             }
 //         },
@@ -86,145 +138,14 @@
 //     }
 // });
 
-//打印
-$("#btn-print").click(function () {
-    jQuery('#fileForm').print();
-})
-//已办表单的datatable
 
-var comForm_stuff = $('#OverTable_Stuff').DataTable({
-    ajax: {
-        url: "/archivedtable_stuff.do",
-    },
-    "order": [[2, 'asc']],
-    "serverSide": true,
-    "columns": [
-        {"data": "ID"},
-        {"data": "TITLE"},
-        {"data": "CREATED_AT"},
-        {"data": null}
-    ],
-    "columnDefs": [
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [3],
-            "render" :  function(data,type,row) {
-                var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='comdetail(this)' value='查看'/>"
-                return html;
-            }
-        },
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [0]
-        }
-    ],
-    "language": {
-        "lengthMenu": "每页_MENU_ 条记录",
-        "zeroRecords": "没有找到记录",
-        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
-        "infoEmpty": "无记录",
-        "search": "搜索：",
-        "infoFiltered": "(从 _MAX_ 条记录过滤)",
-        "paginate": {
-            "previous": "上一页",
-            "next": "下一页"
-        }
-    }
-});
+//待办事务的显示条数
+// setTimeout(function () {
+//     var info=newForm_office.page.info();
+//     $("#nav_num").text(info.recordsTotal)
+// },0);
 
-//已归档事务的datatable
 
-var comForm_office = $('#OverTable_Office').DataTable({
-    ajax: {
-        url: "/archivedtable_office.do",
-    },
-    "order": [[2, 'asc']],
-    "serverSide": true,
-    "columns": [
-        {"data": "OID"},
-        {"data": "CREATED_AT"},
-        {"data": "DEPT"},
-        {"data": "AUTHOR"},
-        {"data": "TITLE"},
-        {"data": null}
-    ],
-    "columnDefs": [
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [5],
-            "render" :  function(data,type,row) {
-                var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='comdetail(this)' value='查看'/>"
-                return html;
-            }
-        },
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [0]
-        }
-    ],
-    "language": {
-        "lengthMenu": "每页_MENU_ 条记录",
-        "zeroRecords": "没有找到记录",
-        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
-        "infoEmpty": "无记录",
-        "search": "搜索：",
-        "infoFiltered": "(从 _MAX_ 条记录过滤)",
-        "paginate": {
-            "previous": "上一页",
-            "next": "下一页"
-        }
-    }
-});
-
-//审批记录的datatable
-
-var comForm_stuff = $('#OverTable_Approval').DataTable({
-    ajax: {
-        url: "/archivedtable_leader.do"
-    },
-    "order": [[2, 'asc']],
-    "serverSide": true,
-    "columns": [
-        {"data": "RID"},
-        {"data": "CREATED_AT"},
-        {"data": "DEPT"},
-        {"data": "AUTHOR"},
-        {"data": "TITLE"},
-        {"data": null}
-    ],
-    "columnDefs": [
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [5],
-            "render" :  function(data,type,row) {
-                var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='comdetail(this)' value='查看'/>"
-                return html;
-            }
-        },
-        {
-            "searchable": false,
-            "orderable": false,
-            "targets": [0]
-        }
-    ],
-    "language": {
-        "lengthMenu": "每页_MENU_ 条记录",
-        "zeroRecords": "没有找到记录",
-        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
-        "infoEmpty": "无记录",
-        "search": "搜索：",
-        "infoFiltered": "(从 _MAX_ 条记录过滤)",
-        "paginate": {
-            "previous": "上一页",
-            "next": "下一页"
-        }
-    }
-});
 
 // 多文件上传
 var fileIndex = 1;
@@ -235,7 +156,6 @@ function add_click_file(index){
 function add(index) {
     /*因为浏览器的设置问题直接用.val()方法取值的时候会取到C:\fakepath\。。所以在这里进行了剪切。*/
     var len = $("#add_file_" + (fileIndex) + "").val().split("\\").length;
-    alert($("#add_file_" + (fileIndex) + "").val().split("\\")[len - 1]);
     var num = $("#add_file_" + (fileIndex) + "").val().split("\\")[len - 1];
     $("#filesUpload").append('<span  id="add_file_span_' + (fileIndex) + '"  class="add_file">' + $("#add_file_" + (fileIndex) + "").val().split("\\")[len - 1] + '</span>');
     $("#filesUpload").append('<a   id="add_file_a_' + (fileIndex) + '"  class="add_file" href="javascript:del_file(' + fileIndex+ ')">删除</a>');
@@ -253,175 +173,98 @@ function del_file(number) {
     o.removeChild(span)
 }
 
-// 清空数据
-function wipeData() {
-    $("#input1").val("");
-    $("#input2").val("");
-    $("#input3").val("");
-    $("#input4").val("");
-    $("#input5").val("");
-    $("#input6").val("");
-    $("#input8").val("");
-    $("#input9").val("");
-    $("#input10").val("");
-    $(".add_file").remove();
-    $("#oId").text("");
-    $("#created_at").text("");
-}
 
 //新建表单 表单放弃
-$("#form_stuff .btn-danger").click(function () {
-        wipeData()
-});
+// $("#form_stuff .btn-danger").click(function () {
+//         wipeData()
+// });
 
 //新建表单 表单关闭 x
-$("#close_stuff").click(function(){
-    wipeData()
-});
+// $("#close_stuff").click(function(){
+//     wipeData()
+// });
 
 // 新建表单 表单保存
-$("#form_stuff .btn-success").click(function () {
-    var dept=  $("#input1").val();
-    var author=  $("#input2").val();
-    var reviewer=  $("#input3").val();
-    var print=  $("#input4").val();
-    var revision=  $("#input5").val();
-    var copy=  $("#input6").val();
-    var keyword=  $("#input8").val();
-    var title=  $("#input9").val();
-    var content=  $("#input10").val();
-    var attachment= $("#filesUpload > span");
-    var arrAttachment=[];
-    var id=$("#oId").text();
-    var created_at=$("#created_at").text();
-    for(var i=0;i<attachment.length;i++){
-        arrAttachment.push(attachment.eq(i).text());
-    }
-    var datas= {
-        "dept":dept,
-        "author":author,
-        "reviewer":reviewer,
-        "print":print,
-        "revision":revision,
-        "copy":copy,
-        "keyword":keyword,
-        "title":title,
-        "content":content,
-        "id":id,
-        "created_at":created_at
-    };
-
-    if(dept == ""){
-        alert("拟稿单位不能为空");
-    }else if(author == ""){
-        alert("拟稿不能为空");
-    }else if(title == ""){
-        alert("标题不能为空");
-    }else if(content == ""){
-        alert("内容不能为空");
-    }else{
-        $.ajax({
-            url: '/saveFormData.do',
-            type: 'post',
-            data: datas,
-            dataType: 'json',
-            async: false,
-            contentType: "application/x-www-form-urlencoded; charset=utf-8",
-            success: function (data) {
-                if(data){
-                    alert("保存成功");
-                }else {
-                    alert("保存失败");
-                }
-                $("#form_stuff").modal("hide");
-                wipeData();
-            },
-            error:function () {
-                alert("系统错误");
-            }
-        });
-    }
-   /* $("#fileForm").submit();*/
-   /*$("#fileForm").ajaxSubmit({
-       url: '/file/multipleUpload.do',
-       type: "post",
-       enctype: 'multipart/form-data',
-       dataType:'json',
-       success: function (data)
-       {
-       },
-       error: function (data)
-       {
-       }
-   })*/
-});
+// $("#form_stuff .btn-success").click(function () {
+//     var dept=  $("#input1").val();
+//     var author=  $("#input2").val();
+//     var reviewer=  $("#input3").val();
+//     var print=  $("#input4").val();
+//     var revision=  $("#input5").val();
+//     var copy=  $("#input6").val();
+//     var keyword=  $("#input8").val();
+//     var title=  $("#input9").val();
+//     var content=  $("#input10").val();
+//     var attachment= $("#filesUpload > span");
+//     var arrAttachment=[];
+//     var id=$("#oId").text();
+//     var created_at=$("#created_at").text();
+//     for(var i=0;i<attachment.length;i++){
+//         arrAttachment.push(attachment.eq(i).text());
+//     }
+//     var datas= {
+//         "dept":dept,
+//         "author":author,
+//         "reviewer":reviewer,
+//         "print":print,
+//         "revision":revision,
+//         "copy":copy,
+//         "keyword":keyword,
+//         "title":title,
+//         "content":content,
+//         "id":id,
+//         "created_at":created_at
+//     };
+//
+//     if(dept == ""){
+//         alert("拟稿单位不能为空");
+//     }else if(author == ""){
+//         alert("拟稿不能为空");
+//     }else if(title == ""){
+//         alert("标题不能为空");
+//     }else if(content == ""){
+//         alert("内容不能为空");
+//     }else{
+//         $.ajax({
+//             url: '/saveFormData.do',
+//             type: 'post',
+//             data: datas,
+//             dataType: 'json',
+//             async: false,
+//             contentType: "application/x-www-form-urlencoded; charset=utf-8",
+//             success: function (data) {
+//                 if(data){
+//                     alert("保存成功");
+//                 }else {
+//                     alert("保存失败");
+//                 }
+//                 $("#form_stuff").modal("hide");
+//                 wipeData();
+//             },
+//             error:function () {
+//                 alert("系统错误");
+//             }
+//         });
+//     }
+// });
 
 //  新建表单 表单提交
 $("#form_stuff .btn-primary").click(function () {
-    var dept=  $("#input1").val();
-    var author=  $("#input2").val();
-    var reviewer=  $("#input3").val();
-    var print=  $("#input4").val();
-    var revision=  $("#input5").val();
-    var copy=  $("#input6").val();
-    var keyword=  $("#input8").val();
-    var title=  $("#input9").val();
-    var content=  $("#input10").val();
-    var attachment= $("#filesUpload > span");
-    var arrAttachment=[];
-    var id=$("#oId").text();
-    var created_at=$("#created_at").text();
-    for(var i=0;i<attachment.length;i++){
-        arrAttachment.push(attachment.eq(i).text());
-    }
-    var datas= {
-        "dept":dept,
-        "author":author,
-        "reviewer":reviewer,
-        "print":print,
-        "revision":revision,
-        "copy":copy,
-        /*"arrAttachment":arrAttachment,*/
-        "keyword":keyword,
-        "title":title,
-        "content":content,
-        "id":id,
-        "created_at":created_at
-    };
-    if(dept == ""){
-        alert("拟稿单位不能为空")
-    }else if(author == ""){
-        alert("拟稿不能为空")
-    }else if(title == ""){
-        alert("标题不能为空")
-    }else if(content == ""){
-        alert("内容不能为空")
-    }else {
-        $.ajax({
-            type: 'post',
-            url: '/submitFormData.do',
-            data: datas,
-            dataType: 'json',
-            async:false,
-            contentType: "application/x-www-form-urlencoded; charset=utf-8",
-            success: function (result) {
-
-                if(result){
-                    alert("提交成功");
-                    newForm_stuff.ajax.url("/nform_stuff.do").load();
-                    submittedForm_stuff.ajax.url("/sform_stuff.do").load();
-                }else {
-                    alert("提交失败");
-                }
-                $("#form_stuff").modal("hide");
-                wipeData();
-            },
-            error:function () {
-                alert("系统错误");
-            }
-        });
-       /* $("#fileForm").submit();*/
-    }
+    // var options  = {
+    //     url:'reveiceFileRegistration.do',
+    //     type:'post',
+    //     success:function(data)
+    //     {
+    //         console.log(data);
+    //         if(data.result == "success"){
+    //             alert("提交成功");
+    //             $('#form_stuff').modal('hide');
+    //         }else {
+    //             alert(data.result);
+    //         }
+    //     }
+    // };
+    // $("#fileForm").ajaxSubmit(options);
 });
 
 // #NewTable_Stuff 表格操作，查看按钮
@@ -535,8 +378,3 @@ function detail_office(that){
 
 }
 
-// var allcookie = document.cookie.split(";");
-// $.each(allcookie,function (i,n) {
-//     console.log(window.sessionStorage)
-// })
-console.log(window.sessionStorage);
