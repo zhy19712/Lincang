fa02c8cb49dd581477dec88715d18f44bbfecf8f
wangguoxin <657826620@qq.com>
2017-11-02 15:20:31 +0800

    后端

---
+++ src/main/java/com/bhidi/lincang/controller/UserManagementController.java
@@ -0,0 +1,31 @@
+package com.bhidi.lincang.controller;
+
+import com.bhidi.lincang.bean.DepartmentAndStaff;
+import com.bhidi.lincang.service.UserManagementServiceImp;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserManagementController {
+    @Autowired
+    UserManagementServiceImp userManagementServiceImp;
+    /**
+     * 获取所有的部门
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value="/getDepartment",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public String getDepartment(){
+        //查询出所有的用户和相对应的角色
+        //List<DepartmentAndStaff> resList=  userManagementServiceImp.getDepartmentAndStaff();
+        //String result = new Gson().toJson(resList);
+        //return result;
+        return "";
+    }
+}
