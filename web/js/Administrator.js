593b18afc457b3ce0c8ea489f76b8890139becf1
WJYman <1615911255@qq.com>
2017-11-07 14:08:23 +0800

    增加用户管理界面

---
+++ web/js/Administrator.js
@@ -0,0 +1,112 @@
+
+
+var all_table = $('#NewTable_Admin').DataTable({
+    ajax: {
+        url: "/sendFileDataTableFirst.do"
+    },
+    "order": [[2, 'asc']],
+    "serverSide": true,
+    "columns": [
+        {"data": "sendfileid"},
+        {"data": "username"},
+        {"data": "name"},
+        {"data": "unit"},
+        {"data": "department"},
+        {"data": null}
+    ],
+    "columnDefs": [
+        {
+            "searchable": false,
+            "orderable": false,
+            "targets": [5],
+            "render" :  function(data,type,row) {
+                var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='edit(this)' value='查看'/>";
+                html += "<input type='button' class='btn btn-warning btn-xs' style='margin-left: 5px;' onclick='edit(this)' value='编辑'/>" ;
+                html += "<input type='button' class='btn btn-danger btn-xs' style='margin-left: 5px;' onclick='edit(this)' value='删除'/>" ;
+                return html;
+            }
+        },
+        {
+            "searchable": false,
+            "orderable": false,
+            "targets": [0]
+        }
+    ],
+    "language": {
+        "lengthMenu": "每页_MENU_ 条记录",
+        "zeroRecords": "没有找到记录",
+        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+        "infoEmpty": "无记录",
+        "search": "搜索：",
+        "infoFiltered": "(从 _MAX_ 条记录过滤)",
+        "paginate": {
+            "previous": "上一页",
+            "next": "下一页"
+        }
+    }
+});
+
+var New_table = $('#NewTable_role').DataTable({
+    ajax: {
+        url: "/sendFileDataTableFirst.do"
+    },
+    "order": [[2, 'asc']],
+    "serverSide": true,
+    "columns": [
+        {"data": "sendfileid"},
+        {"data": "role"},
+        {"data": null}
+    ],
+    "columnDefs": [
+        {
+            "searchable": false,
+            "orderable": false,
+            "targets": [2],
+            "render" :  function(data,type,row) {
+                var html = "<input type='button' class='btn btn-primary btn-xs' style='margin-left: 5px;' onclick='edit(this)' value='查看'/>";
+                html += "<input type='button' class='btn btn-warning btn-xs' style='margin-left: 5px;' onclick='edit(this)' value='编辑'/>" ;
+                html += "<input type='button' class='btn btn-danger btn-xs' style='margin-left: 5px;' onclick='edit(this)' value='删除'/>" ;
+                return html;
+            }
+        },
+        {
+            "searchable": false,
+            "orderable": false,
+            "targets": [0]
+        }
+    ],
+    "language": {
+        "lengthMenu": "每页_MENU_ 条记录",
+        "zeroRecords": "没有找到记录",
+        "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+        "infoEmpty": "无记录",
+        "search": "搜索：",
+        "infoFiltered": "(从 _MAX_ 条记录过滤)",
+        "paginate": {
+            "previous": "上一页",
+            "next": "下一页"
+        }
+    }
+});
+
+
+
+
+
+$(function () {
+
+    //树状复选框插件
+    $("#tree_container").jstree({
+        "plugins" : ["checkbox"],
+
+    });
+    $("#tree_container").jstree().get_selected(true); //获取选中的
+    $('#tree_container').jstree('deselect_all');//全部取消
+    $('#tree_container').jstree('select_all');//全部选中
+
+
+
+})
+
+
+
